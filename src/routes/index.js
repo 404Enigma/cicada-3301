3c95115d15725f4da4f7f1948c7db38c4b365a52
Rahul Mansharamani <rahul.mansharamani.btech2019@sitpune.edu.in>
2021-05-30 05:44:49 +0530

    map view added

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  console.log("we love the earth");
   res.render("pages/index");
 });
 
 router.get("/main", (req, res) => {
-  console.log("we love the earth");
   res.render("pages/main");
 });
 
